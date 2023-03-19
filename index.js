4fb6db52987ccd8c924862b1899b2a19cd6fadd7
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2023-03-19 20:29:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ async function startHisoka() {
   console.log(`using WA v${version.join(".")}, isLatest: ${isLatest}`);
   console.log(
     color(
-      figlet.textSync("Wa-OpenAI", {
+      figlet.textSync("DREADED-AI", {
         font: "Standard",
         horizontalLayout: "default",
         vertivalLayout: "default",

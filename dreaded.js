e64bd898b8dcabbfab074bc69d6e24388d2b1dba
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2023-03-20 19:55:53 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -141,11 +141,6 @@ You may also want to build a bot of this kind, Don't hesitate to text wa.me/2541
           }
         }
 break;
-  case 'attp': case 'ttp': {
-           if (!text) throw `Example : ${prefix + command} text`
-           await dreaded.sendMedia(m.chat, `https://xteam.xyz/${command}?file&text=${text}`, 'dreaded', 'mokaya', m, {asSticker: true})
-
-          break;
         default: {
           if (isCmd2 && budy.toLowerCase() != undefined) {
             if (m.chat.endsWith("broadcast")) return;

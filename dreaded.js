8e778a102ea6cef8b0297bde593c6bb58848e213
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2023-03-23 08:21:51 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const util = require("util");
 const chalk = require("chalk");
 const { Configuration, OpenAIApi } = require("openai");
-let setting = require("./key.json");
+let setting = require("./dreaded.env");
 
 module.exports = dreaded = async (client, m, chatUpdate, store) => {
   try {

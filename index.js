27a073652e1ab61348fef6d708afb2f7a53ad4ee
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2023-09-22 07:42:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -177,7 +177,8 @@ syncFullHistory: true,
       if (!client.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
       if (mek.key.id.startsWith("BAE5") && mek.key.id.length === 16) return;
       m = smsg(client, mek, store);
-      require("./dreaded")(client, m, chatUpdate, store);
+      const dreaded = require("./dreaded");
+dreaded(client, m, chatUpdate, store);
     } catch (err) {
       console.log(err);
     }

0b3340c468b70b4e21542ad1a70a067c2fbafb94
Kresswell <130984721+Kresswell@users.noreply.github.com>
2023-10-12 21:07:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sessionName = "dreaded1";
 const donet = "https://github.com/Fortunatusmokaya/DREADED-AI";
-const owner = ["254114018035"]; // This will send a notification once the bot reconnects
+const owner =["254798242085"]; // This will send a notification once the bot reconnects
 const {
   default: dreadedConnect,
   useMultiFileAuthState,

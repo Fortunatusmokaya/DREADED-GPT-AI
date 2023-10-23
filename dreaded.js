01d0c030db60d9bbb711e7cbedf6a4d61b5abfec
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2023-10-23 19:21:28 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -40,7 +40,9 @@ module.exports = dreaded = async (client, m, chatUpdate, store) => {
 const Heroku = require("heroku-client");  
  const appname = process.env.APP_NAME || '';
  const herokuapi = process.env.HEROKU_API;
+const gptdm = process.env.GPT_INBOX || 'TRUE';
     const cmd = body.startsWith(prefix);
+const autobio = process.env.AUTOBIO || 'TRUE';
     const command = body.replace(prefix, "").trim().split(/ +/).shift().toLowerCase();
     const args = body.trim().split(/ +/).slice(1);
     const pushname = m.pushName || "No Name";
@@ -72,7 +74,7 @@ const Heroku = require("heroku-client");
             const qmsg = (quoted.msg || quoted);
     const author = process.env.STICKER_AUTHOR || 'fortunatus';
     const packname = process.env.STICKER_PACKNAME || 'dreaded';
-const dev = process.env.DEV || '254114018035 254114018035'
+const dev = process.env.DEV || '254114018035'
 
 const menu = process.env.MENU_TYPE || 'VIDEO';
  const DevDreaded = dev.split(",");
@@ -116,9 +118,13 @@ const runtime = function (seconds) {
   client.sendPresenceUpdate('composing', m.chat);
     }
 
+if (autobio === 'TRUE'){ 
+ client.updateProfileStatus(`This bot is active 24/7`).catch(_ => _) 
+         } 
+ 
 
 
-    if (m.chat.endsWith("@s.whatsapp.net")) {
+    if (gptdm === 'TRUE' && m.chat.endsWith("@s.whatsapp.net")) {
 
   	
 
@@ -212,7 +218,7 @@ if (badwordkick === 'TRUE' && isBotAdmin && !isAdmin && body && (new RegExp('\\b
         case "menu":
 let cap = `┌───═[ 𝗗𝗥𝗘𝗔𝗗𝗘𝗗 𝗕𝗢𝗧 ]═──▸
 
-▯ Hello 
+▯ Hello ${m.pushName}
 This is a simple WhatsApp Bot.
 Below is my command list.
 ▯
@@ -543,8 +549,10 @@ case "remove": case "kick": {
  } 
  break;
 
-/*case "vars":
-let vaa = `These are all heroku variables for Dreaded Bot:*/
+case "vars":
+let vaa = `These are all heroku variables for Dreaded Bot:\n\n----REQUIRED VARIABLES----\n\nHEROKU_API\nThis must be fetched and set manually to allow usage of getvar and setvar.\n\nAPP_NAME\nThis is your heroku app name and it MUST be put manually to enable usage of getvar and setvar.\n\nNOT_OWNER_MSG\nMessage bot will send if non-owner uses owner cmd\n\nADMIN_MSG\nMessage sent if admin cmd is used by non-admin\n\nBOT_ADMIN_MSG\nMessage sent if admin cmd is used and bot is not admin\n\nGROUP_ONLY_MSG\nMessage bot sends if a group command is used in private chat.\n\nAI\nopenai API key for chatgpt\n\nDEV\nInput a number to be sudo user.Should be in international format without spaces\n\nSTICKER_AUTHOR\nSticker watermarks\n\nSTICKER_AUTHOR\nSticker watermarks\n\nBAD_WORD\nAny words considered bad\n\nMENU_TYPE\nInput either TEXT, LINK, IMAGE or VIDEO\n\n ✓The set of variables below require you to put TRUE or FALSE\n\n----------\n\nAUTOVIEW\nAUTOBIO\nAUTOREAD_AND_TYPE\nAUTOVIEW_STATUS\nBAD_WORD_KICK\nGPT_INBOX\n\nNOTE:\n -TRUE or FALSE must be in capital letters\n -Wrong inputs will make your bot unresponsive.\n -You can add this vars manually or use getvar and setvar commands.\n -To use getvar/setvar, add the first 2 variables manually first (appname and herokuapi)\n\n---------\n\nContact us incase of issues!`
+reply(vaa)
+break;
 
 case "credits": 
   
